348bf54f2268c0b8f7765a4df05f75b359d1b1d3
gabrieltino <miracle8093@gmail.com>
2019-02-26 10:37:17 +0100

    Mini Laravel Blog with Authentication

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="jumbotron text-center">
+<h1>Welcome to the Laravel</h1>
+<h5>This is the Laravel app from Brad</h5>
+<p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>  <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+</div>
+
+@endsection
